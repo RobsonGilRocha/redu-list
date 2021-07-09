fcd4031b22222841745d6767e7d09f146ffd66c6
RobsonGilRocha <gil1990gil@hotmail.com>
2021-07-09 18:58:38 -0300

    www

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,29 +1,23 @@
 import {useSelector, useDispatch} from 'react-redux'
-import {useState} from 'react'
 
 function Home() {
-    const [ input1, setInput1] = useState()
     const alunos = useSelector( state => state.data);
     const dispatch = useDispatch();
 
-    function addAluno (){
-        dispatch({ type: 'ADD_ALUNO', title: `${input1}`})
-        setInput1("")
+    function ADDTRUE (){
+        dispatch({ type: 'ADD_TRUE'})
     }
-
-    function delAluno (){
-      dispatch({ type: 'DEL_ALUNO'})
-      setInput1("")
+    function ADDFALSE (){
+      dispatch({ type: 'ADD_FALSE'})
   }
 
   return (
       <>
       <ul>
-        {alunos.map (alunos => <li key={alunos}>{alunos}</li>)}
+        {alunos ? <h1>ADD_TRUE</h1>:<h1>ADD_FALSE</h1>}
     </ul>
-    <input value={input1} onChange={(event) => setInput1(event.target.value)}/>
-    <button onClick={addAluno}>ADD</button>
-    <button onClick={delAluno}>DEL</button>
+    <button onClick={ADDTRUE}>ADD_TRUE</button>
+    <button onClick={ADDFALSE}>ADD_FALSE</button>
     </>
   );
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,15 @@
 import { createStore } from 'redux';
 
 const INITIAL_STATE = {
-    data: ["ulo","wwwdwd","kilo"],
+    data: true,
   }
 
   function alunos(state = INITIAL_STATE, action){
         switch (action.type) {
-            case 'ADD_ALUNO' :
-                return { ...state, data: [...state.data, action.title]};
-            case 'DEL_ALUNO' :
-                return { ...state, data: []};
+            case 'ADD_TRUE' :
+                return { data: true};
+            case 'ADD_FALSE' :
+                return { data: false};
                 default:
                     return state;
         }
