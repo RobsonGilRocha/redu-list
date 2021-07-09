ac9f21d13a85797d0e9adfd27d21dcf173870f04
RobsonGilRocha <gil1990gil@hotmail.com>
2021-07-09 20:53:09 -0300

    a

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,22 +2,34 @@ import {useSelector, useDispatch} from 'react-redux'
 
 function Home() {
     const alunos = useSelector( state => state.data);
+    const loading = useSelector( state => state.loading);
     const dispatch = useDispatch();
 
     function ADDTRUE (){
-        dispatch({ type: 'ADD_TRUE'})
+        dispatch({ type: 'DATA_TRUE'})
     }
     function ADDFALSE (){
-      dispatch({ type: 'ADD_FALSE'})
+      dispatch({ type: 'DATA_FALSE'})
+  }
+  function LOADTRUE (){
+    dispatch({ type: 'LOAD_TRUE'})
+  }
+  function LOADFALSE (){
+  dispatch({ type: 'LOAD_FALSE'})
   }
 
   return (
       <>
       <ul>
-        {alunos ? <h1>ADD_TRUE</h1>:<h1>ADD_FALSE</h1>}
+        {alunos ? <h1>DATA_TRUE</h1>:<h1>DATA_FALSE</h1>}
     </ul>
-    <button onClick={ADDTRUE}>ADD_TRUE</button>
-    <button onClick={ADDFALSE}>ADD_FALSE</button>
+    <button onClick={ADDTRUE}>DATA_TRUE</button>
+    <button onClick={ADDFALSE}>DATA_FALSE</button>
+    <ul>
+        {loading ? <h1>loading_TRUE</h1>:<h1>loading_FALSE</h1>}
+    </ul>
+    <button onClick={LOADTRUE}>LOAD_TRUE</button>
+    <button onClick={LOADFALSE}>LOAD_FALSE</button>
     </>
   );
 }
--- src/store/index.js
+++ src/store/index.js
@@ -2,19 +2,24 @@ import { createStore } from 'redux';
 
 const INITIAL_STATE = {
     data: true,
+    loading: false,
   }
 
-  function alunos(state = INITIAL_STATE, action){
+  function truefalse(state = INITIAL_STATE, action){
         switch (action.type) {
-            case 'ADD_TRUE' :
-                return { data: true};
-            case 'ADD_FALSE' :
-                return { data: false};
+            case 'DATA_TRUE' :
+                return { ...state, data: true};
+            case 'DATA_FALSE' :
+                return { ...state, data: false};
+            case 'LOAD_TRUE' :
+                  return {...state, loading: true};
+            case 'LOAD_FALSE' :
+                  return { ...state, loading: false};
                 default:
                     return state;
         }
   }
 
-const store = createStore(alunos)
+const store = createStore(truefalse)
 
 export default store;
