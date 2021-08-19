c88f71b5e456ddad7fdbf816397ea548ab123923
RobsonGilRocha <gil1990gil@hotmail.com>
2021-08-19 19:34:01 -0300

    18082021

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,7 +2,16 @@ import {useSelector, useDispatch} from 'react-redux'
 
 function Home() {
     const alunos = useSelector( state => state.data);
+    const login = useSelector( state => state.login);
+    const loginType = useSelector( state => state.loginType);
+    const powerOn = useSelector( state => state.powerOn);
+    const colorDark = useSelector( state => state.colorDark);
     const loading = useSelector( state => state.loading);
+    const colorPrimary = useSelector( state => state.colorPrimary);
+    const colorSecondary = useSelector( state => state.colorSecondary);
+    const error = useSelector( state => state.error);
+    const errorMsg = useSelector( state => state.errorMsg);
+
     const dispatch = useDispatch();
 
     function ADDTRUE (){
@@ -30,6 +39,23 @@ function Home() {
     </ul>
     <button onClick={LOADTRUE}>LOAD_TRUE</button>
     <button onClick={LOADFALSE}>LOAD_FALSE</button>
+    <ul>
+        {login ? <h1>login _TRUE</h1>:<h1>login _FALSE</h1>}
+        <h1>loginType _{loginType}</h1>
+        <input   />
+    </ul>
+    <ul>
+        {powerOn ? <h1>powerOn _TRUE</h1>:<h1>powerOn _FALSE</h1>}
+    </ul>
+    <ul>
+        {error ? <h1>error _TRUE</h1>:<h1>error _FALSE</h1>}
+        {errorMsg ? <h1>errorMsg _TRUE</h1>:<h1>errorMsg _FALSE</h1>}
+    </ul>
+    <ul>
+        {colorDark ? <h1>colorDark _TRUE</h1>:<h1>colorDark _FALSE</h1>}
+        {colorPrimary ? <h1>colorPrimary _TRUE</h1>:<h1>colorPrimary _FALSE</h1>}
+        {colorSecondary ? <h1>colorSecondary _TRUE</h1>:<h1>colorSecondary _FALSE</h1>}
+    </ul>
     </>
   );
 }
--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,14 @@ import { createStore } from 'redux';
 const INITIAL_STATE = {
     data: true,
     loading: false,
+    login: false,
+    loginType: `regular`,
+    powerOn: false,
+    colorDark: false,
+    colorPrimary: false,//sem uso no action
+    colorSecondary: false,//sem uso no action
+    error: false,
+    errorMsg: `one`, //sem uso no action
   }
 
   function truefalse(state = INITIAL_STATE, action){
@@ -12,9 +20,23 @@ const INITIAL_STATE = {
             case 'DATA_FALSE' :
                 return { ...state, data: false};
             case 'LOAD_TRUE' :
-                  return {...state, loading: true};
+                return { ...state, loading: true};
             case 'LOAD_FALSE' :
-                  return { ...state, loading: false};
+                return { ...state, loading: false};
+            case 'LOGIN_DEV_TRUE' :
+                  return {...state, loginType: `loginTypeDev`};
+            case 'POWER_ON_FALSE' :
+                  return { ...state, powerOn: false};
+            case 'POWER_ON_TRUE' :
+                  return { ...state, powerOn: true};
+            case 'COLORDARK_FALSE' :
+                  return { ...state, colorDark: false};
+            case 'COLORDARK_TRUE' :
+                  return { ...state, colorDark: true};
+            case 'ERROR_FALSE' :
+                  return { ...state, error: false};
+            case 'ERROR_TRUE' :
+                  return { ...state, error: true};
                 default:
                     return state;
         }
